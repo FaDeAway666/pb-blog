2957861051ac280b8713234feca5b9ca1a917d1a
wangxi <wangxi@wangxideMacBook-Pro.local>
2023-02-08 18:31:10 +0800

    feat: update component menu

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -7,8 +7,15 @@ export default function Layout({ children }) {
     <>
       <Header />
       <div className="relative bg-gray-100">
-        <Menu />
-        <main className="max-w-screen-lg bg-white ml-72 min-h-[calc(100vh-9rem)]">{children}</main>
+        <div className="absolute w-64 float-left left-4 top-8">
+          <Menu
+            data={[
+              { id: '1', label: 'aa' },
+              { id: '2', label: 'bb', children: [{ id: '2-1', label: 'bb1' }] }
+            ]}
+          />
+        </div>
+        <main className="max-w-screen-lg bg-white mx-72 min-h-[calc(100vh-9rem)]">{children}</main>
       </div>
       <Footer />
     </>
--- components/navigate/menu.js
+++ components/navigate/menu.js
@@ -1,3 +1,39 @@
-export default function Menu({ props }) {
-  return <div className="absolute w-64 float-left left-4 top-8">menu</div>
+import PropTypes from 'prop-types'
+import IconArrowRight from '~icons/ant-design/right-outlined.jsx'
+
+export default function Menu(props) {
+  const { data } = props
+  return (
+    <ul className="w-100">
+      {data.map(item => {
+        if (item.children?.length > 0) {
+          return (
+            <li key={item.id}>
+              <div className="flex aligns-center">
+                <IconArrowRight />
+                <span>{item.label}</span>
+              </div>
+              <Menu data={item.children}></Menu>
+            </li>
+          )
+        } else {
+          return (
+            <li>
+              <span>{item.label}</span>
+            </li>
+          )
+        }
+      })}
+    </ul>
+  )
+}
+
+Menu.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.objectOf({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      children: PropTypes.array
+    })
+  )
 }
