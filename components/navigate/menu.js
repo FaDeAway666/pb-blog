dc231dd9fe9a97a3179309d9c402f5cda1a053d4
wangxi <wangxi@wangxideMacBook-Pro.local>
2023-02-15 18:34:28 +0800

    feat: menu update

--- components/navigate/menu.js
+++ components/navigate/menu.js
@@ -10,78 +10,84 @@ const MenuContext = React.createContext({
   setSelected: () => {}
 })
 
-export default function Menu(props) {
-  const { data, level, onSelect } = props
-  let lv = level ? parseInt(level) || 0 : 0
+function Menu(props) {
+  const { data, level } = props
+  let lv = level || 0
 
   const [expand, setExpand] = useState(false)
-  const [current, setCurrent] = useState(null)
-
+  // const [current, setCurrent] = useState(null)
   const { selected, setSelected } = useContext(MenuContext)
 
   const handleSelect = (e, item) => {
-    setCurrent(item)
-    setSelected(item)
-    onSelect &&
-      onSelect({
-        domEvent: e,
-        selectedItem: current
-      })
+    // setCurrent(item)
+    setSelected({
+      domEvent: e,
+      selectedItem: item
+    })
   }
 
-  const showSelectedStyle = item => {
-    if (selected?.id === item.id) return SELECTED_STYLE
-    else if (lv === 0 && current?.id === item.id) return SELECTED_STYLE
-    else return
+  // const showSelectedStyle = item => {
+  //   if (selected?.id === item.id) return SELECTED_STYLE
+  //   else if (lv === 0 && current?.id === item.id) return SELECTED_STYLE
+  //   else return
+  // }
+
+  return (
+    <ul>
+      {data.map(item => {
+        if (item.children?.length > 0) {
+          return (
+            <li key={item.id} className={`leading-8 mb-1 ${!expand ? COLLAPSE_STYLE : ''}`}>
+              <div className={`relative rounded px-6 hover:bg-gray-200`} onClick={() => setExpand(!expand)}>
+                <IconArrowRight
+                  className={`absolute left-0 top-1.5 transition-transform ${expand ? 'rotate-90' : 'rotate-0'}`}
+                />
+                <span className={`pl-${lv * 6} `}>{item.label}</span>
+              </div>
+
+              <Menu data={item.children} level={lv + 1}></Menu>
+            </li>
+          )
+        } else {
+          return (
+            <li key={item.id} className="leading-8 mb-1">
+              <div
+                className={`relative rounded px-6 hover:bg-gray-200 ${selected?.id === item.id ? SELECTED_STYLE : ''}`}
+                onClick={e => handleSelect(e, item)}
+              >
+                <span className={`pl-${lv * 6} `}>{item.label}</span>
+              </div>
+            </li>
+          )
+        }
+      })}
+    </ul>
+  )
+}
+
+Menu.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  level: PropTypes.number
+}
+
+export default props => {
+  const { data, onSelect } = props
+
+  const [current, setCurrent] = useState(null)
+  const triggerSelect = data => {
+    let { selectedItem } = data
+    setCurrent(selectedItem)
+    onSelect && onSelect(data)
   }
 
   return (
     <MenuContext.Provider
       value={{
         selected: current,
-        setSelected: setCurrent
+        setSelected: triggerSelect
       }}
     >
-      <ul>
-        {data.map(item => {
-          if (item.children?.length > 0) {
-            return (
-              <li key={item.id} className={`leading-8 mb-1 ${!expand ? COLLAPSE_STYLE : ''}`}>
-                <div className={`relative rounded px-6 hover:bg-gray-200`} onClick={() => setExpand(!expand)}>
-                  <IconArrowRight
-                    className={`absolute left-0 top-1.5 transition-transform ${expand ? 'rotate-90' : 'rotate-0'}`}
-                  />
-                  <span className={`pl-${lv * 6} `}>{item.label}</span>
-                </div>
-
-                <Menu
-                  data={item.children}
-                  level={lv + 1}
-                  onSelect={data => {
-                    console.log(data)
-                    onSelect && onSelect(data)
-                  }}
-                ></Menu>
-              </li>
-            )
-          } else {
-            return (
-              <li key={item.id} className="leading-8 mb-1">
-                <div
-                  className={`relative rounded px-6 hover:bg-gray-200 ${showSelectedStyle(item)}`}
-                  onClick={e => handleSelect(e, item)}
-                >
-                  <span className={`pl-${lv * 6} `}>{item.label}</span>
-                </div>
-              </li>
-            )
-          }
-        })}
-      </ul>
+      <Menu data={data} />
     </MenuContext.Provider>
   )
-}
-
-Menu.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object)
 }
